01679d39ab766536bfa4b70c31bfbfc54ce1d630
Аксенова Дарья <aksenova@lanit.ru>
2023-04-26 17:35:25 +0300

    feat: les2 dz

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -5,4 +5,20 @@ import {Component} from '@angular/core';
 	templateUrl: './products-list.component.html',
 	styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+	title = 'Shiba Inu';
+	subtitle = 'Dog Breed';
+	text =
+		'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.';
+	img = 'https://material.angular.io/assets/img/examples/shiba2.jpg';
+
+	onClickLike(event: MouseEvent) {
+		event.stopPropagation();
+		console.log('Like');
+	}
+
+	onClickShare(event: MouseEvent) {
+		event.stopPropagation();
+		console.log('Share');
+	}
+}
