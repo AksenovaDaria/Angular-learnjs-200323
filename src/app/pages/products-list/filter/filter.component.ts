2058455f77789fa7f0adccf82617416a9ea214fc
Аксенова Дарья <aksenova@lanit.ru>
2023-06-07 14:27:22 +0300

    feat: lesson11-HW

--- src/app/pages/products-list/filter/filter.component.ts
+++ src/app/pages/products-list/filter/filter.component.ts
@@ -10,9 +10,9 @@ import {
 } from '@angular/core';
 import {IProductsFilter} from './products-filter.interface';
 import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
-import {map, takeUntil} from 'rxjs';
+import {first, map, takeUntil} from 'rxjs';
 import {DestroyService} from '../../../shared/destroy/destroy.service';
-import {IProductsFilterForm} from './products-filter-form.interface';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
 	selector: 'app-filter',
@@ -21,88 +21,112 @@ import {IProductsFilterForm} from './products-filter-form.interface';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	providers: [DestroyService],
 })
-export class FilterComponent {
+export class FilterComponent implements OnChanges, OnInit {
 	@Input() brands!: string[] | null;
 
 	@Output() changeFilter = new EventEmitter<IProductsFilter>();
 
-	// readonly filterForm = new FormGroup({
-	// 	name: new FormControl('123', {validators: [Validators.minLength(3)]}),
-	// 	brands: new FormArray<FormControl<boolean>>([]),
-	// 	priceRange: new FormGroup({
-	// 		min: new FormControl(0),
-	// 		max: new FormControl(999999),
-	// 	}),
-	// })
+	readonly filterForm = new FormGroup({
+		name: new FormControl('', {validators: [Validators.minLength(3)]}),
+		brands: new FormArray<FormControl<boolean>>([]),
+		priceRange: new FormGroup({
+			min: new FormControl(0),
+			max: new FormControl(999999),
+		}),
+	});
 
-	// constructor(
-	// 	private readonly destroy$: DestroyService,
-	// ) {}
+	constructor(
+		private readonly destroy$: DestroyService,
+		private route: ActivatedRoute,
+	) {}
 
-	// ngOnChanges({brands}: SimpleChanges) {
-	// 	if (brands) {
-	// 		this.initBrandsForm();
-	// 	}
-	// }
-
-	// ngOnInit() {
-	// 	this.listenFilterChange();
-	// }
-
-	// get nameFormControl(): FormControl {
-	// 	return this.filterForm.get('name') as FormControl;
-	// }
-
-	// get minFormControl(): FormControl {
-	// 	return this.filterForm.get(['priceRange', 'min']) as FormControl;
-	// }
-
-	// get maxFormControl(): FormControl {
-	// 	return this.filterForm.get(['priceRange', 'max']) as FormControl;
-	// }
-
-	// private initBrandsForm() {
-	// 	const brandsControls = this.brands
-	// 		? this.brands.map(() => new FormControl<boolean>(false))
-	// 		: [] as FormControl<boolean>[];
-	// 	const brandsForm = new FormArray(brandsControls) as FormArray<FormControl<boolean>>;
-
-	// 	this.filterForm.setControl('brands', brandsForm);
-	// }
-
-	// private listenFilterChange() {
-	// 	this.filterForm.valueChanges.pipe(
-	// 		map(({brands, ...formValue}): IProductsFilter => ({
-	// 			...formValue,
-	// 			brands: this.getBrandsListFromForm(brands as boolean[]),
-	// 		}) as IProductsFilter),
-	// 		takeUntil(this.destroy$),
-	// 	).subscribe(filter => {
-	// 		this.changeFilter.emit(filter);
-	// 	})
-	// }
-
-	// private getBrandsListFromForm(brandsFormValue: boolean[]): IProductsFilter['brands'] {
-	// 	return !this.brands
-	// 		? []
-	// 		: this.brands.filter((_, index) => brandsFormValue[index]);
-	// }
-
-	onFilterSubmit({name, priceRange, brands}: IProductsFilterForm) {
-		const filter: IProductsFilter = {
-			name: name,
-			priceRange: priceRange,
-			brands: this.getBrandsListFromForm(brands),
-		};
-
-		this.changeFilter.emit(filter);
+	ngOnChanges({brands}: SimpleChanges) {
+		if (brands) {
+			this.initBrandsForm();
+		}
+	}
+	private initBrandsForm() {
+		const queryBrands: any = [];
+		this.route.queryParamMap.pipe(first()).subscribe((data: any) => {
+			queryBrands.push(data.params.brands);
+		});
+		const brandsControls = this.brands
+			? this.brands.map(brand => {
+					let indexBoolean = 0;
+					for (const key in queryBrands[0]) {
+						if (brand == queryBrands[0][key]) {
+							indexBoolean = 1;
+						}
+					}
+					if (indexBoolean === 1) {
+						return new FormControl<boolean>(true);
+					} else {
+						return new FormControl<boolean>(false);
+					}
+			  })
+			: ([] as FormControl<boolean>[]);
+		const brandsForm = new FormArray(brandsControls) as FormArray<
+			FormControl<boolean>
+		>;
+		this.filterForm.setControl('brands', brandsForm);
+	}
+	ngOnInit() {
+		this.route.queryParamMap.pipe(first()).subscribe((data: any) => {
+			if (data.params.name) {
+				this.filterForm.controls['name'].setValue(data.params.name);
+			}
+			if (data.params.minPrice) {
+				this.filterForm.controls['priceRange'].controls['min'].setValue(
+					data.params.minPrice,
+				);
+			}
+			if (data.params.maxPrice) {
+				this.filterForm.controls['priceRange'].controls['max'].setValue(
+					data.params.maxPrice,
+				);
+			}
+		});
+		this.listenFilterChange();
 	}
 
-	private getBrandsListFromForm(
-		brandsFormValue: Record<string, boolean>,
-	): IProductsFilter['brands'] {
+	private listenFilterChange() {
+		this.filterForm.valueChanges
+			.pipe(
+				map(
+					({brands, ...formValue}): IProductsFilter =>
+						({
+							...formValue,
+							brands: this.getBrandsListFromForm(brands as boolean[]),
+						} as IProductsFilter),
+				),
+				takeUntil(this.destroy$),
+			)
+			.subscribe(filter => {
+				this.changeFilter.emit(filter);
+			});
+	}
+
+	private getBrandsListFromForm(brandsFormValue: boolean[]): IProductsFilter['brands'] {
 		return !this.brands
 			? []
 			: this.brands.filter((_, index) => brandsFormValue[index]);
 	}
+
+	// onFilterSubmit({name, priceRange, brands}: IProductsFilterForm) {
+	// 	const filter: IProductsFilter = {
+	// 		name: name,
+	// 		priceRange: priceRange,
+	// 		brands: this.getBrandsListFromForm(brands),
+	// 	};
+
+	// 	this.changeFilter.emit(filter);
+	// }
+
+	// private getBrandsListFromForm(
+	// 	brandsFormValue: Record<string, boolean>,
+	// ): IProductsFilter['brands'] {
+	// 	return !this.brands
+	// 		? []
+	// 		: this.brands.filter((_, index) => brandsFormValue[index]);
+	// }
 }
